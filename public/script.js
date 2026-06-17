b2e7443e7906537d5742f544f3ea5b246a0f326b
muk1m0v <mukimov.shop@gmail.com>
2026-06-18 01:06:30 +0500

    Глобальное улучшение изображение тем

--- public/script.js
+++ public/script.js
@@ -51,10 +51,10 @@ function applyTheme(t) {
     theme = t;
     if (t === 'dark') {
         document.documentElement.setAttribute('data-theme', 'dark');
-        themeIcon.src = 'sun.png'; // при тёмной показываем солнце (чтобы переключить на светлую)
+        themeIcon.src = '/img/sun.png'; // при тёмной показываем солнце (чтобы переключить на светлую)
     } else {
         document.documentElement.removeAttribute('data-theme');
-        themeIcon.src = 'moon.png'; // при светлой показываем луну (чтобы переключить на тёмную)
+        themeIcon.src = '/img/moon.png'; // при светлой показываем луну (чтобы переключить на тёмную)
     }
 }
 
